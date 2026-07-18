22eda79e29a1a0d22b2e1594847e6df1b092222b
Nilakshi2006 <Nilakshibhanot@gmail.com>
2026-07-18 14:27:49 +0530

    02-folder complete

--- 02-folder/src/App.jsx
+++ 02-folder/src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import Card from '../src/Components/Card'
+import Navbar from '../src/Components/Navbar'
 const App = () => {
   return (
     <div>
+      <Navbar/>
       <Card/>
     </div>
   )
--- 02-folder/src/Components/Card.jsx
+++ 02-folder/src/Components/Card.jsx
@@ -4,9 +4,9 @@ const Card = () => {
   return (
     <div>
 
-    <div className="Card">
-      <h1>Hey</h1>
-      <p>Im Nilakshi</p>
+    <div className='Card'>
+ <h1>Nilakshi</h1>
+ <h3>Computer Science Student from Hoshiarpur having strongfoundation in html,css and js.Now im enhancing my skills in React.js </h3>
     </div>
     </div>
   )
