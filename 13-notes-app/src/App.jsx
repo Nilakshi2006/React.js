7945b0a691d43583d3e92d5ddc8580933aa2ce74
Nilakshi2006 <Nilakshibhanot@gmail.com>
2026-08-07 21:51:04 +0530

    still in progress

--- 13-notes-app/src/App.jsx
+++ 13-notes-app/src/App.jsx
@@ -1,44 +1,64 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const App = () => {
-  const submitHandler=(e)=>{
+  const [title, setTitle] = useState('')
+  const [details, setDetails] = useState('')
+  const submitHandler = (e) => {
     e.preventDefault();
-    console.log("Form Submitted");
-  }
-  return (
-    <div
-    className="h-screen bg-black text-white lg:flex"
-    >
-      <form onSubmit={(e)=>{
-submitHandler(e)
-      }}
-      className="lg:w-1/2 items-start flex flex-col gap-4 p-10">
-       
-          <input 
-        className="px-5 py-2 border-2 rounded w-full outline-none font-medium"
-        type="text"
-         placeholder="Enter Notes Heading">
+    console.log(title);
+    console.log(details)
+    setTitle('')
+    setDetails('')
+  };
 
-         </input>
-        <textarea 
-        className="px-5 py-2 border-2 rounded h-30 w-full outline-none font-medium"
-        type="text" 
-        placeholder="Enter Notes Detail"
-          >
-        </textarea>
-        <button
-        className="bg-white text-black px-5 py-2 border-2 rounded w-full outline-none">
+  return (
+    <div className="h-screen bg-black text-white lg:flex">
+
+      <form
+        onSubmit={(e) => {
+          submitHandler(e);
+        }}
+        className="lg:w-1/2 items-start flex flex-col gap-4 p-10"
+      >
+
+         <h1 className="text-4xl font-bold">Add Notes</h1>
+
+        <input
+          className="px-5 py-2 border-2 rounded w-full outline-none font-medium"
+          type="text"
+          placeholder="Enter Notes Heading"
+          value={title}
+          onChange={(e)=>{
+            setTitle(e.target.value)
+          }}
+        ></input>
+
+        <textarea
+          className="px-5 py-2 border-2 rounded h-30 w-full outline-none font-medium"
+          type="text"
+          placeholder="Enter Notes Detail"
+          value={details}
+          onChange={(e)=>{
+setDetails(e.target.value)
+          }}
+        ></textarea>
+
+        <button className="bg-white text-black px-5 py-2 border-2 rounded w-full outline-none">
           Add Note
         </button>
-        
+
       </form>
-      <div className="flex flex-wrap p-10 lg:w-1/2 bg-gray-900">
-        <div className="h-32 w-32 rounded-2xl bg-white">
+      <div className=" p-10 lg:w-1/2 lg:border-l-2 ">
+        <h1 className="text-4xl font-bold">Recent Notes</h1>
+        <div className="flex flex-wrap gap-5 mt-5 h-full overflow-auto">
+          <div className="h-50 w-37 rounded-2xl bg-white"></div>
+          <div className="h-50 w-37 rounded-2xl bg-white"></div>
+            <div className="h-50 w-37 rounded-2xl bg-white"></div>
+          
+        </div>
       </div>
-
-</div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
